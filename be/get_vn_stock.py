7b29c08edb94caf3fde49ba1320f21d1d4fb80f3
thinh.bui <thinh.bui@gotecq.com>
2023-04-27 11:24:57 +0700

    add: clone vn stock data

--- be/get_vn_stock.py
+++ be/get_vn_stock.py
@@ -1,5 +1,30 @@
 import vnquant.data as dt
-loader = dt.DataLoader('VND', '2018-02-02', '2018-04-02')
 
-data = loader.download()
-data.to_csv("vnd.csv")
+COMPANIES = [
+    "ACB", "BCM", "BID", "BVH", "CTG",
+    "FPT", "GAS", "GVR", "HDB", "HPG",
+    "MBB", "MSN", "MWG", "NVL", "PDR",
+    "PLX", "POW", "SAB", "SSI", "STB",
+    "TCB", "TPB", "VCB", "VHM", "VIB",
+    "VIC", "VJC", "VNM", "VPB", "VRE"
+]
+
+TRAIN_START_DATE = "2012-01-01"
+TRAIN_END_DATE = "2020-12-31"
+TEST_START_DATE = "2021-01-01"
+TEST_END_DATE = "2023-04-14"
+
+""" Get train data """
+# for company in COMPANIES:
+#     loader = dt.DataLoader(company, TRAIN_START_DATE, TRAIN_END_DATE)
+
+#     data = loader.download()
+#     data.to_csv(f"vn_train_data/{company.lower()}.csv")
+
+
+""" Get test data """
+# for company in COMPANIES:
+#     loader = dt.DataLoader(company, TEST_START_DATE, TEST_END_DATE)
+
+#     data = loader.download()
+#     data.to_csv(f"vn_test_data/{company.lower()}.csv")
