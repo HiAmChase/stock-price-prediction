7afd5cb807d7c72fb928825656e5086389cdf3d8
HiAmChase <ngocthinh303@gmail.com>
2023-04-30 11:17:44 +0700

    chore: remove redundant file

--- fe/src/api/config.js
+++ fe/src/api/config.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
-const instance = axios.create({
-  baseURL: "http://localhost:5000",
-})
+const baseURL = "http://localhost:5000"
+
+const instance = axios.create({ baseURL })
 
 export default instance
