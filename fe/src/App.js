1f93469a25f7e47180353709a45590dc83436307
HiAmChase <ngocthinh303@gmail.com>
2023-05-08 21:32:55 +0700

    feat: fundamental

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import ChartDisplay from "./components/ChartDisplay"
 import Market from "./components/Market"
+import Fundament from "./components/Fundament"
 import "./App.css"
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       </div>
       <div className="App__rightPanel">
         <Market />
+        <Fundament />
       </div>
     </div>
   )
---
+++ fe/src/components/Fundament.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react"
+
+import "./Fundament.css"
+
+function NumberFormat({ n, type }) {
+  const [num, setNum] = useState("-")
+
+  useEffect(() => {
+    if (type === "pct") {
+      setNum(`${(n * 100).toFixed(2)}%`)
+    }
+  }, [n])
+
+  return <span className={n > 0 ? `Value__up` : `Value__down`}>{num}</span>
+}
+
+function Fundament() {
+  return (
+    <div className="Fundament">
+      <div className="Fundgeneral">
+        <h4>Company Name</h4>
+        {/* Title */}
+        <div>
+          Industry
+          <i className="fa-solid fa-circle separatedot"></i>
+          Sector
+          <i className="fa-solid fa-circle separatedot"></i>
+          Country
+        </div>
+        {/* Price */}
+        <div>
+          <h1>
+            34.45
+            <span className="chg">
+              <NumberFormat n={0.2} type="pct" />
+            </span>
+          </h1>
+          <span className="Fund__text">Prev Close: {(0.3).toFixed(2)}</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Fundament
