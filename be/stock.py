f2e907591359576439411a4248a6985fe1625ef5
HiAmChase <ngocthinh303@gmail.com>
2023-05-09 21:55:40 +0700

    update: stock dât

--- be/stock.py
+++ be/stock.py
@@ -1,240 +1,422 @@
 STOCK = [
     {
         "ticker": "aapl",
-        "company_name": "Apple Inc."
+        "company_name": "Apple Inc.",
+        "industry": "Technology",
+        "sector": "Electronics",
+        "country": "USA"
     },
     {
         "ticker": "axp",
-        "company_name": "American Express Company"
+        "company_name": "American Express Company",
+        "industry": "Financial",
+        "sector": "Services",
+        "country": "USA"
     },
     {
         "ticker": "ba",
-        "company_name": "The Boeing Company"
+        "company_name": "The Boeing Company",
+        "industry": "Aerospace",
+        "sector": "Manufacture",
+        "country": "USA"
     },
     {
         "ticker": "cat",
-        "company_name": "Caterpillar Inc."
+        "company_name": "Caterpillar Inc.",
+        "industry": "Financial",
+        "sector": "Services",
+        "country": "USA"
     },
     {
         "ticker": "csco",
-        "company_name": "Cisco Systems Inc."
+        "company_name": "Cisco Systems Inc.",
+        "industry": "Technology",
+        "sector": "Electronics",
+        "country": "USA"
     },
     {
         "ticker": "cvx",
-        "company_name": "Chevron Corporation"
+        "company_name": "Chevron Corporation",
+        "industry": "Energy",
+        "sector": "Resources",
+        "country": "USA"
     },
     {
         "ticker": "dd",
-        "company_name": "DuPont de Nemours Inc."
+        "company_name": "DuPont de Nemours Inc.",
+        "industry": "Chemicals",
+        "sector": "Manufacture",
+        "country": "USA"
     },
     {
         "ticker": "dis",
-        "company_name": "The Walt Disney Company"
+        "company_name": "The Walt Disney Company",
+        "industry": "Entertainment",
+        "sector": "Entertainment",
+        "country": "USA"
     },
     {
         "ticker": "ge",
-        "company_name": "General Electric Company"
+        "company_name": "General Electric Company",
+        "industry": "Energy",
+        "sector": "Resources",
+        "country": "USA"
     },
     {
         "ticker": "gs",
-        "company_name": "The Goldman Sachs Group, Inc."
+        "company_name": "The Goldman Sachs Group, Inc.",
+        "industry": "Financial",
+        "sector": "Services",
+        "country": "USA"
     },
     {
         "ticker": "hd",
-        "company_name": "The Home Depot, Inc."
+        "company_name": "The Home Depot, Inc.",
+        "industry": "Retail",
+        "sector": "Services",
+        "country": "USA"
     },
     {
         "ticker": "ibm",
-        "company_name": "International Business Machines Corporation"
+        "company_name": "International Business Machines Corporation",
+        "industry": "Technology",
+        "sector": "Electronics",
+        "country": "USA"
     },
     {
         "ticker": "intc",
-        "company_name": "Intel Corporation"
+        "company_name": "Intel Corporation",
+        "industry": "Technology",
+        "sector": "Electronics",
+        "country": "USA"
     },
     {
         "ticker": "jnj",
-        "company_name": "Johnson & Johnson"
+        "company_name": "Johnson & Johnson",
+        "industry": "Healthcare",
+        "sector": "Manufacture",
+        "country": "USA"
     },
     {
         "ticker": "jpm",
-        "company_name": "JPMorgan Chase & Co."
+        "company_name": "JPMorgan Chase & Co.",
+        "industry": "Financial",
+        "sector": "Services",
+        "country": "USA"
     },
     {
         "ticker": "ko",
-        "company_name": "The Coca-Cola Company"
+        "company_name": "The Coca-Cola Company",
+        "industry": "Beverage",
+        "sector": "Consumer Staples",
+        "country": "USA"
     },
     {
         "ticker": "mcd",
-        "company_name": "McDonald's Corporation"
+        "company_name": "McDonald's Corporation",
+        "industry": "Food",
+        "sector": "Services",
+        "country": "USA"
     },
     {
         "ticker": "mmm",
-        "company_name": "3M Company"
+        "company_name": "3M Company",
+        "industry": "Healthcare",
+        "sector": "Manufacture",
+        "country": "USA"
     },
     {
         "ticker": "mrk",
-        "company_name": "Merck & Co., Inc."
+        "company_name": "Merck & Co., Inc.",
+        "industry": "Healthcare",
+        "sector": "Manufacture",
+        "country": "USA"
     },
     {
         "ticker": "msft",
-        "company_name": "Microsoft Corporation"
+        "company_name": "Microsoft Corporation",
+        "industry": "Technology",
+        "sector": "Electronics",
+        "country": "USA"
     },
     {
         "ticker": "nke",
-        "company_name": "NIKE, Inc."
+        "company_name": "NIKE, Inc.",
+        "industry": "Athletic Footwear",
+        "sector": "Consumer Staples",
+        "country": "USA"
     },
     {
         "ticker": "pfe",
-        "company_name": "Pfizer Inc."
+        "company_name": "Pfizer Inc.",
+        "industry": "Healthcare",
+        "sector": "Manufacture",
+        "country": "USA"
     },
     {
         "ticker": "pg",
-        "company_name": "The Procter & Gamble Company"
+        "company_name": "The Procter & Gamble Company",
+        "industry": "Product",
+        "sector": "Manufacture",
+        "country": "USA"
     },
     {
         "ticker": "rtx",
-        "company_name": "Raytheon Technologies Corporation"
+        "company_name": "Raytheon Technologies Corporation",
+        "industry": "Aerospace",
+        "sector": "Manufacture",
+        "country": "USA"
     },
     {
         "ticker": "trv",
-        "company_name": "The Travelers Companies"
+        "company_name": "The Travelers Companies",
+        "industry": "Insurance",
+        "sector": "Services",
+        "country": "USA"
     },
     {
         "ticker": "unh",
-        "company_name": "UnitedHealth Group Incorporated"
+        "company_name": "UnitedHealth Group Incorporated",
+        "industry": "Healthcare",
+        "sector": "Manufacture",
+        "country": "USA"
     },
     {
         "ticker": "v",
-        "company_name": "Visa Inc."
+        "company_name": "Visa Inc.",
+        "industry": "Financial",
+        "sector": "Services",
+        "country": "USA"
     },
     {
         "ticker": "vz",
-        "company_name": "Verizon Communications Inc."
+        "company_name": "Verizon Communications Inc.",
+        "industry": "Telecommunications",
+        "sector": "Services",
+        "country": "USA"
     },
     {
         "ticker": "wmt",
-        "company_name": "Walmart Inc."
+        "company_name": "Walmart Inc.",
+        "industry": "Retail",
+        "sector": "Services",
+        "country": "USA"
     },
     {
         "ticker": "xom",
-        "company_name": "Exxon Mobil Corporation"
+        "company_name": "Exxon Mobil Corporation",
+        "industry": "Energy",
+        "sector": "Resources",
+        "country": "USA"
     },
     {
         "ticker": "acb",
-        "company_name": "Ngân hàng TMCP Á Châu"
+        "company_name": "Ngân hàng TMCP Á Châu",
+        "industry": "Financial",
+        "sector": "Services",
+        "country": "VNM"
     },
     {
         "ticker": "bcm",
-        "company_name": "Tổng Công ty Đầu tư và Phát triển Công nghiệp"
+        "company_name": "Tổng Công ty Đầu tư và Phát triển Công nghiệp",
+        "industry": "Industry",
+        "sector": "Development",
+        "country": "VNM"
     },
     {
         "ticker": "bid",
-        "company_name": "Ngân hàng TMCP Đầu tư và Phát triển Việt Nam"
+        "company_name": "Ngân hàng TMCP Đầu tư và Phát triển Việt Nam",
+        "industry": "Financial",
+        "sector": "Services",
+        "country": "VNM"
     },
-    {"ticker": "bvh",
-     "company_name": "Tập đoàn Bảo Việt"
-     },
+    {
+        "ticker": "bvh",
+        "company_name": "Tập đoàn Bảo Việt",
+        "industry": "Financial",
+        "sector": "Services",
+        "country": "VNM"
+    },
     {
         "ticker": "ctg",
-        "company_name": "Ngân hàng TMCP Công Thương Việt Nam - VIETINBANK"
+        "company_name": "Ngân hàng TMCP Công Thương Việt Nam - VIETINBANK",
+        "industry": "Financial",
+        "sector": "Services",
+        "country": "VNM"
     },
     {
         "ticker": "fpt",
-        "company_name": "Công ty Cổ phần FPT"
+        "company_name": "Công ty Cổ phần FPT",
+        "industry": "Technology",
+        "sector": "Services",
+        "country": "VNM"
     },
     {
         "ticker": "gas",
-        "company_name": "Tổng Công ty Khí Việt Nam"
+        "company_name": "Tổng Công ty Khí Việt Nam",
+        "industry": "Energy",
+        "sector": "Resources",
+        "country": "VNM"
     },
     {
         "ticker": "gvr",
-        "company_name": "Tập đoàn Công nghiệp Cao su Việt Nam"
+        "company_name": "Tập đoàn Công nghiệp Cao su Việt Nam",
+        "industry": "Industry",
+        "sector": "Development",
+        "country": "VNM"
     },
     {
         "ticker": "hdb",
-        "company_name": "Ngân hàng TMCP Phát triển - HDBank"},
+        "company_name": "Ngân hàng TMCP Phát triển - HDBank",
+        "industry": "Financial",
+        "sector": "Services",
+        "country": "VNM"
+    },
     {
         "ticker": "hpg",
-        "company_name": "CTCP Tập đoàn Hòa Phát"
+        "company_name": "CTCP Tập đoàn Hòa Phát",
+        "industry": "Industry",
+        "sector": "Development",
+        "country": "VNM"
     },
     {
         "ticker": "mbb",
-        "company_name": "Ngân hàng TMCP Quân Đội - MBBank"
+        "company_name": "Ngân hàng TMCP Quân Đội - MBBank",
+        "industry": "Financial",
+        "sector": "Services",
+        "country": "VNM"
     },
     {
         "ticker": "msn",
-        "company_name": "CTCP Tập đoàn Masan"
+        "company_name": "CTCP Tập đoàn Masan",
+        "industry": "Consumer Goods",
+        "sector": "Services",
+        "country": "VNM"
     },
     {
         "ticker": "mwg",
-        "company_name": "CTCP Đầu tư Thế giới Di động"
+        "company_name": "CTCP Đầu tư Thế giới Di động",
+        "industry": "Technology",
+        "sector": "Services",
+        "country": "VNM"
     },
     {
         "ticker": "nvl",
-        "company_name": "Tập đoàn Đầu tư Địa ốc NoVa"
+        "company_name": "Tập đoàn Đầu tư Địa ốc NoVa",
+        "industry": "Investment",
+        "sector": "Commercial",
+        "country": "VNM"
     },
     {
         "ticker": "pdr",
-        "company_name": "CTCP Phát triển Bất động sản Phát Đạt"
+        "company_name": "CTCP Phát triển Bất động sản Phát Đạt",
+        "industry": "Investment",
+        "sector": "Commercial",
+        "country": "VNM"
     },
     {
         "ticker": "plx",
-        "company_name": "Tập đoàn Xăng Dầu Việt Nam"
+        "company_name": "Tập đoàn Xăng Dầu Việt Nam",
+        "industry": "Energy",
+        "sector": "Resources",
+        "country": "VNM"
     },
     {
         "ticker": "pow",
-        "company_name": "Tổng Công ty Điện lực Dầu khí Việt Nam"
+        "company_name": "Tổng Công ty Điện lực Dầu khí Việt Nam",
+        "industry": "Energy",
+        "sector": "Resources",
+        "country": "VNM"
     },
     {
         "ticker": "sab",
-        "company_name": "CTCP Bia - Rượu - Nước giải khát Sài Gòn"
+        "company_name": "CTCP Bia - Rượu - Nước giải khát Sài Gòn",
+        "industry": "Beverage",
+        "sector": "Consumer Staples",
+        "country": "VNM"
     },
     {
         "ticker": "ssi",
-        "company_name": "CTCP Chứng khoán SSI"
+        "company_name": "CTCP Chứng khoán SSI",
+        "industry": "Financial",
+        "sector": "Services",
+        "country": "USA"
     },
     {
         "ticker": "stb",
-        "company_name": "Ngân hàng TMCP Sài Gòn Thương Tín - SacomBank"
+        "company_name": "Ngân hàng TMCP Sài Gòn Thương Tín - SacomBank",
+        "industry": "Financial",
+        "sector": "Services",
+        "country": "VNM"
     },
     {
         "ticker": "tcb",
-        "company_name": "Ngân hàng TMCP Kỹ thương Việt Nam - Techcombank"
+        "company_name": "Ngân hàng TMCP Kỹ thương Việt Nam - Techcombank",
+        "industry": "Financial",
+        "sector": "Services",
+        "country": "VNM"
     },
     {
         "ticker": "tpb",
-        "company_name": "Ngân hàng TMCP Tiên Phong - TPBank"
+        "company_name": "Ngân hàng TMCP Tiên Phong - TPBank",
+        "industry": "Financial",
+        "sector": "Services",
+        "country": "VNM"
     },
     {
         "ticker": "vcb",
-        "company_name": "Ngân hàng TMCP Ngoại thương Việt Nam - Vietcombank"
+        "company_name": "Ngân hàng TMCP Ngoại thương Việt Nam - Vietcombank",
+        "industry": "Financial",
+        "sector": "Services",
+        "country": "VNM"
     },
     {
         "ticker": "vhm",
-        "company_name": "Công ty Cổ phần Vinhomes"
+        "company_name": "Công ty Cổ phần Vinhomes",
+        "industry": "Estate",
+        "sector": "Investment",
+        "country": "VNM"
     },
     {
         "ticker": "vib",
-        "company_name": "Ngân hàng TMCP Quốc tế Việt Nam - VietstockFinance"
+        "company_name": "Ngân hàng TMCP Quốc tế Việt Nam - VietstockFinance",
+        "industry": "Financial",
+        "sector": "Services",
+        "country": "VNM"
     },
     {
         "ticker": "vic",
-        "company_name": "Tập đoàn VINGROUP"
+        "company_name": "Tập đoàn VINGROUP",
+        "industry": "Business",
+        "sector": "Services",
+        "country": "VNM"
     },
     {
         "ticker": "vjc",
-        "company_name": "CTCP Hàng không Vietjet"
+        "company_name": "CTCP Hàng không Vietjet",
+        "industry": "Airline",
+        "sector": "Services",
+        "country": "VNM"
     },
     {
         "ticker": "vnm",
-        "company_name": "CTCP Sữa Việt Nam - Vinamilk"
+        "company_name": "CTCP Sữa Việt Nam - Vinamilk",
+        "industry": "Beverage",
+        "sector": "Consumer Staples",
+        "country": "VNM"
     },
     {
         "ticker": "vpb",
-        "company_name": "Ngân hàng TMCP Việt Nam Thịnh Vượng - VPBank"
+        "company_name": "Ngân hàng TMCP Việt Nam Thịnh Vượng - VPBank",
+        "industry": "Financial",
+        "sector": "Services",
+        "country": "VNM"
     },
     {
         "ticker": "vre",
-        "company_name": "CTCP Vincom Retail"
+        "company_name": "CTCP Vincom Retail",
+        "industry": "Retail",
+        "sector": "Services",
+        "country": "VNM"
     },
 ]
