9764b17f50b6816a1eb901bfe3fb4f7df0e57a51
Thinh Bui Ngoc <thinh.bui@gotecq.com>
2023-05-20 11:59:36 +0700

    update: styling

--- fe/src/App.js
+++ fe/src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import ChartDisplay from "./components/ChartDisplay"
 import Market from "./components/Market"
 import Fundament from "./components/Fundament"
+import StockTable from "./components/StockTable"
 import "./App.css"
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
       </div>
       <div className="App__bottomPanel">
         <Market />
-        <div className="temp"></div>
+        <StockTable />
         <Fundament />
       </div>
     </div>
---
+++ fe/src/components/StockTable.js
@@ -0,0 +1,75 @@
+import React from "react"
+import "./StockTable.css"
+
+function StockTable() {
+  return (
+    <div className="stock-table">
+      <table class="table table-hover custom-table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default StockTable
