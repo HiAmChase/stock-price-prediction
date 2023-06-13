3afe53a370f607f3ea6b84b8f3bc4f5a233e3bfa
HiAmChase <ngocthinh303@gmail.com>
2023-06-13 22:03:40 +0700

    add: new tooltip

--- fe/src/components/GrownRate.js
+++ fe/src/components/GrownRate.js
@@ -12,6 +12,20 @@ function GrownRate({
   return (
     <div className="GrownRate">
       <div className="GrownRate__title_header">
+        <div className="GrownRate__tooltip">
+          <Tooltip title="The data will be automatically updated at 00:00pm">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-info-circle-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
+            </svg>
+          </Tooltip>
+        </div>
         <h4 className="GrownRate__title">Top Highest Growth Rate</h4>
         <div className="GrownRate__group">
           <div className="GrownRate__dropdown">
